a27bc5be1c2aa13e7d4b689564736904a2c124f7
Arman Mamian <armanmamian@gmail.com>
2021-08-16 20:21:46 +0400

    Fix typo in ft_freematrix

--- libft/ft_freematrix.c
+++ libft/ft_freematrix.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_freematrix.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aisraely <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: amamian <amamian@student.42yerevan.am>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 14:18:28 by aisraely          #+#    #+#             */
-/*   Updated: 2021/07/27 14:18:31 by aisraely         ###   ########.fr       */
+/*   Updated: 2021/08/16 18:58:45 by amamian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_freematrix(char ***matrix)
+void	ft_freematrix(char **matrix)
 {
 	int	i;
 
-	i = ft_matrixlen(*matrix) - 1;
+	i = ft_matrixlen(matrix) - 1;
 	while (i >= 0)
-		free((*matrix)[i--]);
-	free(*matrix);
+		free((matrix)[i--]);
+	free(matrix);
 }
