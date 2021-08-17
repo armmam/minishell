994bc63a0d17e7f370eee164c9fd1522ab296d7e
Arman Mamian <armanmamian@gmail.com>
2021-08-17 20:49:54 +0400

    Fix ft_dmtxclear

--- libft/ft_dmtxclear.c
+++ libft/ft_dmtxclear.c
@@ -6,7 +6,7 @@
 /*   By: amamian <amamian@student.42yerevan.am>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 18:49:21 by amamian           #+#    #+#             */
-/*   Updated: 2021/08/04 18:58:48 by amamian          ###   ########.fr       */
+/*   Updated: 2021/08/17 19:44:05 by amamian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@
  */
 void	ft_dmtxclear(t_dmtx *dmtx)
 {
+	int		i;
+
+	i = 0;
+	while (i < dmtx->len)
+		free(dmtx->ptr[i++]);
 	free(dmtx->ptr);
 	free(dmtx);
 }
