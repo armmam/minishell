0f93f10a62e5b888f21335ad7f93309a3e5a6943
Arman Mamian <armanmamian@gmail.com>
2021-08-19 22:36:10 +0400

    Add more tests

--- test/test_tokenize.c
+++ test/test_tokenize.c
@@ -39,9 +39,20 @@ void	test_tokenize(void)
 	t_darr	*expected;
 
 	expected = ft_darrnew(10);
-	ft_darrpushback(expected, "<<");
-	ft_darrpushback(expected, "an");
+	ft_darrpushback(expected, ft_strdup("<<"));
+	ft_darrpushback(expected, ft_strdup("an"));
 	TEST_ASSERT_EQUAL_STRING(expected->ptr[0], "<<");
 	TEST_ASSERT_EQUAL_STRING(expected->ptr[1], "an");
 	TEST_ASSERT_EQUAL_STRING_ARRAY(expected->ptr, ft_tokenize("   <<an    "), 2);
+	TEST_ASSERT_EQUAL_STRING_ARRAY(expected->ptr, ft_tokenize(" <<   an    "), 2);
+	TEST_ASSERT_EQUAL_STRING_ARRAY(expected->ptr, ft_tokenize(" <<  an"), 2);
+	ft_darrclear(expected);
+
+	expected = ft_darrnew(10);
+	ft_darrpushback(expected, ft_strdup("|"));
+	ft_darrpushback(expected, ft_strdup("|||"));
+	TEST_ASSERT_EQUAL_STRING_ARRAY(expected->ptr, ft_tokenize("| |||"), 2);
+	TEST_ASSERT_EQUAL_STRING_ARRAY(expected->ptr, ft_tokenize("  \t  | |||"), 2);
+	ft_darrclear(expected);
+
 }
